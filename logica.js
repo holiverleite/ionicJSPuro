8c1f3f7cec0075e4284728b52570334a48a656c9
Haroldo Leite <holiverleite@gmail.com>
2019-05-14 20:33:58 -0300

    Implementado metodos no javascript para manipular pessoa e departamento

--- logica.js
+++ logica.js
@@ -1,29 +1,35 @@
-const gasto = document.querySelector("#input-gasto");
-const valor = document.querySelector("#input-valor");
-const limpar = document.querySelector("#btn-limpar");
-const enviar = document.querySelector("#btn-enviar");
-const lista = document.querySelector("#lista-gastos");
-const gastosTotaisInput = document.querySelector("#gastosTotais");
+const inputDepartamento = document.querySelector("#input-departamento");
+const inputFuncionario = document.querySelector("#input-funcionario");
+const inputValor = document.querySelector("#input-valor");
+const limparDepartamento = document.querySelector("#btn-limpar-departamento");
+const limparPessoa = document.querySelector("#btn-limpar-pessoa");
+const cadastrarDepartamento = document.querySelector("#btn-cadastrar-departamento");
+const cadastrarPessoa = document.querySelector("#btn-adicionar-pessoa");
+const selectorDepartamento = document.querySelector("#selector-departamento");
+const listaFuncionarios = document.querySelector("#lista-funcionarios");
+
 //Estou selecionando o elemento pelo nome, e nÃ£o pelo ID
 const alertCtrl = document.querySelector('ion-alert-controller');
 
-let gastosTotais = 0;
-
-function limparForm(){
-    valor.value = "";
-    gasto.value = "";
+function limparFormDepartamento(){
+    inputDepartamento.value = "";
+    inputFuncionario.value = "";
+    inputValor.value = "";
 }
 
-enviar.addEventListener('click', () => {
-    //Valores dos inputs
-    const valorInformado = valor.value;
-    const gastoInformado = gasto.value;
-    //Validar os inputs
-    if(gastoInformado.trim().lenght <= 0 ||  valorInformado <=0 ){
-        console.log("Informe valores vÃ¡lidos");
+function limparFormPessoa(){
+    inputFuncionario.value = "";
+    inputValor.value = "";
+}
+
+cadastrarDepartamento.addEventListener('click', () => {
+    const dep = inputDepartamento.value;
+
+    if(dep == ""){
+        console.log("Informe valores válidos");
         //Diferente de todo o resto, 'create' nÃ£o Ã© vanilla js 
         alertCtrl.create({
-            message: 'Entre um gasto e um valor',
+            message: 'Entre com o nome de um departamento para realizar o cadastro',
             header: 'Invalid Inputs',
             buttons: ['Ok']
         }).then(alertElement => {
@@ -31,14 +37,41 @@ enviar.addEventListener('click', () => {
         });
         return
     }
+
     //console.log(gastoInformado + ': ',valorInformado);
-    const novoGasto = document.createElement('ion-item');
-    novoGasto.textContent = gastoInformado + ': R$' + valorInformado;
-    lista.appendChild(novoGasto);
-    //Atualiza valor total
-    gastosTotais = gastosTotais + +valor.value;
-    gastosTotaisInput.textContent = gastosTotais;
-    limparForm();
+    const novoDepartamento = document.createElement('ion-select-option');
+    novoDepartamento.textContent = dep;
+    selectorDepartamento.appendChild(novoDepartamento);
+    
+    limparFormDepartamento();
 })
 
-limpar.addEventListener('click',limparForm)
+
+cadastrarPessoa.addEventListener('click', () => {
+    const nomeFunc = inputFuncionario.value;
+    const salFunc = inputValor.value;
+    const depart = selectorDepartamento.value;
+
+    if(nomeFunc == "" || salFunc == "" || depart == ""){
+        console.log("Informe valores válidos");
+        //Diferente de todo o resto, 'create' nÃ£o Ã© vanilla js 
+        alertCtrl.create({
+            message: 'Entre com o nome de um funcionario e um salário para realizar o cadastro',
+            header: 'Invalid Inputs',
+            buttons: ['Ok']
+        }).then(alertElement => {
+            alertElement.present();
+        });
+        return
+    }
+
+    const novoFuncionario = document.createElement('ion-item');
+    novoFuncionario.textContent = "Funcionario: " + nomeFunc + "\n" + "Salario: " + salFunc + "\n" + "Departamento: " + depart;
+    listaFuncionarios.appendChild(novoFuncionario);
+    
+    limparFormPessoa();
+})
+
+limparPessoa.addEventListener('click',limparFormPessoa)
+
+limparDepartamento.addEventListener('click',limparFormDepartamento)
